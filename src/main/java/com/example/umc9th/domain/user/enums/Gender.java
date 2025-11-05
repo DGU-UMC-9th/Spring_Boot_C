ee42d22ddf45f3eebd182bc5b7b37654de1cc91d
Juri-Jang <jjr84@dgu.ac.kr>
2025-11-05 23:16:41 +0900

    fix/불필요 enum 제거

--- src/main/java/com/example/umc9th/domain/user/enums/Gender.java
+++ src/main/java/com/example/umc9th/domain/user/enums/Gender.java
@@ -2,8 +2,4 @@ package com.example.umc9th.domain.user.enums;
 
 public enum Gender {
     MALE, FEMALE, UNKNOWN;
-
-    public enum TermsEnum {
-        AGE, SERVICE, PRIVACY, LOCATION, MARKETING
-    }
 }
