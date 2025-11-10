72e3caf111cf1e2e6725c8add61bea6d82214e5b
fbehdgus <danny90697@gmail.com>
2025-11-10 19:24:40 +0900

    Feat: Repository에 QueryDsl 추가(#6)

--- src/main/java/com/example/demo/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/demo/domain/review/repository/ReviewRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long>,ReviewQueryDsl {
 
     @Query("SELECT r FROM Review r JOIN FETCH r.member m WHERE r.store.id = :storeId")
     List<Review> findAllByStoreIdWithMember(@Param("storeId") Long storeId);
