f524e1491582e6e92560307d2505f666aa2cf308
seohyunk09 <2022112400@dgu.ac.kr>
2025-11-18 20:12:28 +0900

    mission/#7 ApiResponse

--- src/main/java/com/example/umc9th2/global/apiPayload/ApiResponse.java
+++ src/main/java/com/example/umc9th2/global/apiPayload/ApiResponse.java
@@ -29,8 +29,6 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> onSuccess(BaseSuccessCode code) {
         return new ApiResponse<>(true, code.getCode(), code.getMessage(), null);
     }
-
-
     // 실패한 경우 (result 포함)
     public static <T> ApiResponse<T> onFailure(BaseErrorCode code, T result) {
         return new ApiResponse<>(false, code.getCode(), code.getMessage(), result);
